27d4c3c5b3832c3124369e594f9a71401cacb4cf
Egor <marderosov7@gmail.com>
2021-03-21 00:45:06 +0200

    Task-2

--- hw-1/hw-1.js
+++ hw-1/hw-1.js
@@ -1,13 +1,31 @@
 // TASK 1
-function bitNum(number) {
-    let tmp = (number).toString(2)
-    let count = 0;
-    for(let i in tmp) {
-        if(tmp[i]==1) {
-            count++
-        } 
-    }
-    console.log(count)
-}
+// function bitNum(number) {
+//     let tmp = (number).toString(2)
+//     let count = 0;
+//     for(let i in tmp) {
+//         if(tmp[i]==1) {
+//             count++
+//         } 
+//     }
+//     console.log(count)
+// }
 
 // TASK 2 
+function sortString(string) {
+    if(string.length==0) {
+        return ""
+    }
+    else {
+        let result = []
+        let arrString = string.split(" ")
+        for ( let value of arrString ){
+            for (let element of value){
+                if(!isNaN(element)) {
+                    // result.splice(element-1,0,value)
+                    result[element-1]=value
+                }
+            }
+        }
+        console.log(result)
+    }
+}
