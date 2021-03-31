603c0bfff81b6f573cfc62693d3ac53472478e2f
Egor <marderosov7@gmail.com>
2021-03-31 21:28:44 +0300

    Added hw-8

---
+++ hw-8/hw-8.js
@@ -0,0 +1,39 @@
+
+function countdown(ms) {
+
+    let res = [];
+    let tmp = [];
+
+    const hr = ms / 3600000;
+    const min = ms % 3600000 / 60000;
+    const sec = ms % 360000  % 60000 / 1000;
+
+    tmp.push(parseInt(hr), parseInt(min), parseInt(sec))
+
+    res = tmp.map((el,index,arr) => {
+
+        if(ms < 0) {
+            el = el * -1
+        }
+
+        if(el.toString().length < 2) {
+            el = "0" + el;
+        }
+
+        return el;
+    })
+
+    if(ms < 0) {
+        res[0] = "-" + res[0];
+    }
+
+    else {
+        res[0] = "+" + res[0];
+    }
+
+    return res.join(":")
+}
+
+
+console.log(countdown(-61000))
+
