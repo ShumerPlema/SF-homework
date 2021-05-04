8b7e4aee183ec455d6f082cf5474253331aabd2b
ShumerPlema <40871531+ShumerPlema@users.noreply.github.com>
2021-05-04 18:12:42 +0300

    Delete dem-2 directory

--- dem-2/routes/notFoundRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const notFoundRoutes = express.Router();
-const { notFoundPage } = require("../controllers/notFound/notFoundControllers.js")
-
-notFoundRoutes.get("/", notFoundPage)
-
-module.exports = notFoundRoutes;
